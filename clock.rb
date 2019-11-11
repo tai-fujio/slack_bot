fcdf35e01c638270d42616a3326a820e55dc4617
taisaburo_fujio <taisaburo.fujio@gmail.com>
2019-11-11 14:57:58 +0900

    gemfileインストール、slackにメッセージ機能・clorckworkで自動送信機能実装

---
+++ clock.rb
@@ -0,0 +1,13 @@
+require 'clockwork'
+require 'active_support/time'
+require './sample.rb'
+include Clockwork
+
+module Clockwork
+  handler do |job,time|
+    puts "Running #{job},at #{time}"
+  end
+  every(5.seconds, 'send a message to slack_bot_app') do
+    send_a_message
+  end
+end
